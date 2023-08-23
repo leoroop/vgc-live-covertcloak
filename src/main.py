8e719f40dbe0d52c9efb264b50061fa5233f2076
leoroop <leon.berta@gmail.com>
2023-08-23 19:06:46 +0200

    Save current status

--- src/main.py
+++ src/main.py
@@ -28,13 +28,13 @@ def main():
         # frame = cv2.imread("../screens/change.png")
         # frame = cv2.imread("../screens/teampreview.png")
         success, frame = capture.read()
-        
-        clean = frame.copy()        
-        process(frame, covers)
-        
-        # show_markers(clean)
 
-        cv2.imshow("VGC Hide Info (Beta)", frame)
+        clean = frame.copy()        
+        # process(frame, covers)
+        
+        show_markers(clean)
+
+        # cv2.imshow("VGC Hide Info (Beta)", frame)
         cv2.imshow("Cleanfeed", clean)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
